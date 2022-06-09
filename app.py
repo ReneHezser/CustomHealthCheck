45d46d5fb9637d90ebb3b4d03db339ac455bc5be
René Hézser <rehezser@microsoft.com>
2022-06-09 10:35:54 +0200

    extracted file write

--- app.py
+++ app.py
@@ -3,13 +3,15 @@ import random
 
 READ_INTERVAL = 10 #seconds
 
+def writeStatus(status):
+    f = open("/app/system-status.txt", "w")
+    f.write(status)
+    f.close()
+
 def run():
     # set healthy state (0 means healthy)
     print("Starting and writing health check to 'system-status.txt'")
-    f = open("/app/system-status.txt", "w")
-    f.write("0")
-    f.close()
-    print("done")
+    writeStatus("0")
 
     next_call = time.time()
 
@@ -20,15 +22,10 @@ def run():
         if random.randrange(1, 10) == 5:
             print("Simulating failure")
             # set healthy state (1 means unhealthy)
-            f = open("/app/system-status.txt", "w")
-            f.write("1")
-            f.close()
+            writeStatus("1")
 
         print("sleeping...")
         # sleep until the next read cycle by calculating the time between the next desired call and now
         time.sleep(next_call - time.time())
-        # quit()
 
-# if __name__ == '__main__':
-print("Starting up...")
 run()  
